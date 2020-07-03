634c4594d5e5c68f94df5db25ebeade44fdf8c95
Your Name <you@example.com>
2020-07-03 20:26:29 +0300

    change name of project

--- ImageEditor/ImageEditor/wsgi.py
+++ ImageEditor/ImageEditor/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ImageEdintor project.
+WSGI config for ImageEditor project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ImageEdintor.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ImageEditor.settings')
 
 application = get_wsgi_application()
